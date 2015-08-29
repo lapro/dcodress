c6824ef9ff8b66c7a19d4820a70e2684d5ea467b
isa9x <iza_helena93@yahoo.co.id>
2015-08-29 19:38:15 +0700

    1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,21 +17,13 @@
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      
+        </div>     
     </nav>
 
 <!-- Carousel
     ================================================== -->
     <div class="row">
-    	<div class="col-lg-8">
+    	<div class="col-lg-9">
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
@@ -43,7 +35,7 @@
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      {!! Html::image('assets/gallery/1440676859278.jpg','First Picture', array('style'=>'width:990px; height:500px;')) !!}
+      {!! Html::image('assets/gallery/1440676859278.jpg','First Picture', array('style'=>'width:100%; height:500px;')) !!}
       <div class="carousel-caption">
         ...
       </div>
